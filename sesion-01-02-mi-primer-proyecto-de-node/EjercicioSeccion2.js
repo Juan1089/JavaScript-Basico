1e81f9e3a91f8cb2096b64944911dd8556a3a0e1
Juan Manuel <juanrecchimuzzi@hotmail.com>
2022-09-29 17:58:55 -0300

    Ejercicio2

---
+++ sesion-01-02-mi-primer-proyecto-de-node/EjercicioSeccion2.js
@@ -0,0 +1,13 @@
+const lista = ["Juan", 39, true, "April 20 1983", "Gladiador"]
+console.log(lista)
+
+const libroFavorito = {
+    titulo: "Gladiador", 
+    autor: "Raul Lopez", 
+    fecha:"March 13 1995", 
+    url: "https://campus.open-bootcamp.com/cursos/15/leccion/1749",
+}
+console.log(libroFavorito.titulo)
+console.log(libroFavorito.autor)
+console.log(libroFavorito.fecha)
+console.log(libroFavorito.url)
