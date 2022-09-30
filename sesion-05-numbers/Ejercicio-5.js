c756626960b414e1c12986f22a021f2e40676ecf
Juan Manuel <juanrecchimuzzi@hotmail.com>
2022-09-30 01:15:52 -0300

    Ejercicio 5

---
+++ sesion-05-numbers/Ejercicio-5.js
@@ -0,0 +1,11 @@
+let miAlturaEnCentimitros = 168;
+let miAlturaDeMetros = 1.6855;
+let miPeso = 62.311;
+
+console.log(miAlturaDeMetros.toFixed(2));
+
+console.log(miPeso.toFixed(2));
+
+let max_valor_JS = Number.MAX_VALUE;
+console.log(max_valor_JS, 1);
+
