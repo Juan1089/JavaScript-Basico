8dd1dcd5e6bba1a14f5556be53ae38910cac6a39
Juan Manuel <juanrecchimuzzi@hotmail.com>
2022-10-03 19:12:26 -0300

    a

---
+++ sesion-07-trabajando-con-otros-tipos-de-objetos/conjuntos.js
@@ -0,0 +1,11 @@
+const array = ["Gabriela", "Veronica", "Elsa", "Juan"]
+
+const miSet = new Set(array)
+
+console.log(array)
+console.log(miSet)
+
+miSet.add("Juan")
+console.log(miSet)
+miSet.add("Javascript")
+console.log(miSet)
